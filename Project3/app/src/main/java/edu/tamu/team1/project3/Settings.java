9c79aa8727903cd880aab8e0e0f06326b24eafc1
Jacob Stone <jacobstone@Jacobs-MacBook-Pro.local>
2014-11-25 23:48:39 -0600

    Having a problem with the colors but worked on settings

--- Project3/app/src/main/java/edu/tamu/team1/project3/Settings.java
+++ Project3/app/src/main/java/edu/tamu/team1/project3/Settings.java
@@ -1,9 +1,36 @@
 package edu.tamu.team1.project3;
 
-public class Settings {
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+public class Settings {
+    public Settings(){}
+    public String getSettingsXML(String file) throws Exception{
+        String xml = null;
+
+        DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+        InputSource is = new InputSource(file);
+        Document document = docBuilderFactory.newDocumentBuilder().parse(is);
+
+        StringWriter sw = new StringWriter();
+        Transformer serializer = TransformerFactory.newInstance().newTransformer();
+        serializer.transform(new DOMSource(document), new StreamResult(sw));
+
+        xml = sw.toString();
+        return xml;
+    }
     // Functions used for getting and setting the Fling setting for the game
-    public void setFling(boolean x){
+    public void setFling(boolean x) throws Exception{
 
     }
     public boolean getFling(){
@@ -21,9 +48,16 @@ public class Settings {
 
     }
 
-    public String getTheme(){
+    public String getSettingsTheme() throws Exception{
 
-        return null;
+        String xml = getSettingsXML("settings.xml");
+        DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        InputSource src = new InputSource();
+        src.setCharacterStream(new StringReader(xml));
+
+        Document doc = builder.parse(src);
+        String theme = doc.getElementsByTagName("theme").item(0).getTextContent();
+        return theme;
     }
     // Functions used for getting and setting the topic of the game
     public void setTopic(String topic){
