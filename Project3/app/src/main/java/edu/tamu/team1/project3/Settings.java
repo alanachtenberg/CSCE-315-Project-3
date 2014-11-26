5764bf8144a9a6f2543f7eb69357c79c3c860c17
Jacob Stone <jacobstone@Jacobs-MacBook-Pro.local>
2014-11-25 22:16:34 -0600

    work in progress: Settings Class and XML

---
+++ Project3/app/src/main/java/edu/tamu/team1/project3/Settings.java
@@ -0,0 +1,35 @@
+package edu.tamu.team1.project3;
+
+public class Settings {
+
+    // Functions used for getting and setting the Fling setting for the game
+    public void setFling(boolean x){
+
+    }
+    public boolean getFling(){
+        return false;
+    }
+    // Functions used for getting and setting the Swipe setting for the game
+    public void setSwipe(boolean x){
+
+    }
+    public boolean getSwipe(){
+        return false;
+    }
+    // Functions used for getting and setting the theme of the game
+    public void setTheme(String theme){
+
+    }
+
+    public String getTheme(){
+
+        return null;
+    }
+    // Functions used for getting and setting the topic of the game
+    public void setTopic(String topic){
+
+    }
+    public String getTopic(){
+        return null;
+    }
+}
